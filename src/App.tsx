cdf11b95f18b3c8898ee4d815e4a8af7f0e4f515
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-03-16 21:28:51 -0400

    feat: setup dashboard to render components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { DashBoard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <DashBoard />
+      <GlobalStyle />
+    </>
+  );
+}
+
