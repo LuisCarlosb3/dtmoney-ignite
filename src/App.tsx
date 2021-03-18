ef3385986370656438a7d609653983c47939e69f
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-03-18 19:40:44 -0400

    feat: ensure CreateNewTransactionModal works correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import React, { useState } from "react";
 import { DashBoard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
-
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true)
+  }
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <DashBoard />
+        <NewTransactionModal 
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+          />
       <GlobalStyle />
     </>
   );
